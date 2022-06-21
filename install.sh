7023524e5c4053e692db628930348b90d6adfbde
Chris Wahlfeldt <chrisw@pixotech.com>
2022-06-21 17:15:10 -0500

    edits

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # audio
-xi alsa-utils
+sudo xbps-install alsa-utils
 sudo touch /etc/asound.conf
 sudo echo "defaults.pcm.card 1" >> /etc/asound.conf
 sudo echo "defaults.pcm.device 0" >> /etc/asound.conf
@@ -12,21 +12,53 @@ sudo echo "add_drivers+=\"bcm5974\"" > /etc/dracut.conf.d/10-touchpad.conf
 sudo dracut --force
 
 # fans
-xi mbpfan
-sudo ln -s /etc/sv/mbpfan /var/service
+sudo xbps-install -S mbpfan
+sudo ln -s /etc/sv/mbpfan /var/service/
 mbpfan -t
 
 # lid hibernate
-sudo ln -s /etc/sv/acpid /var/service
+sudo ln -s /etc/sv/acpid /var/service/
 
 # powersave
-xi thermald powertop
+sudo xbps-install -S thermald powertop
 sudo ln -s /etcsv/thermald /var/service
 sudo powertop -auto-tune
 
 # microcode updates
-sudo xbps-install -S intel-ucode
+cd ~/void-packages
+./void-packages pkg intel-ucode
+sudo xbps-install --repository=hostdir/binpkgs/nonfree intel-ucode
 sudo xbps-reconfigure -f linux
+cd ~/
 
 # fun stuff
-sudo xbps-install xorg-minimal dejavu-font-ttf xf86-video-intel
+sudo xbps-install -S \
+    base-devel \
+    xorg-server \
+    xorg-minimal \
+    xinit \
+    xf86-video-intel \
+    xf86-input-libinput \
+    picom \
+    dejavu-font-ttf \
+    noto-fonts-ttf \
+    nerd-fonts-ttf \
+    font-awesome \
+    firefox \
+    qutebrowser \
+    kitty \
+    feh \
+    dmenu \
+    exa
+
+sudo xbps-install -S nodejs-lts
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+# Add to .bash_profile
+export PATH=~/.npm-global/bin:$PATH
+source ~/.bash_profile
+
+sudo ln -sf /etc/sv/alsa /var/service
+sudo ln -sf /etc/sv/dbus /var/service
+
+
