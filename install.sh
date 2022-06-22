89e15cd44877722d200816dffad3fdf3031983a5
Chris Wahlfeldt <chrisw@pixotech.com>
2022-06-22 10:48:25 -0500

    wip

--- install.sh
+++ install.sh
@@ -1,13 +1,43 @@
 #! /bin/bash
 
+# first steps:
+# login as root
+# groupadd sudo
+# visudo -> uncomment sudo group
+# usermod -a -G sudo waffles
+# reboot and log in as waffles and run this script
+
+cd ~/
+
+sudo wpa_passphrase "waffleiron" "wlp0s20u1" >> /etc/wpa_supplicant/wpa_supplicant.conf
+sudo wpa_supplicant -B - i wlp0s20u1 -c /etc/wpa_supplicant/wpa_supplicant.conf
+
+sudo xbps-install -Sy git
+
+git clone https://github.com/void-linux/void-packages.git
+cd void-packages
+./xbps-src binary-bootstrap
+echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
+
+./xbps-src pkg xtools
+sudo xbps-install -y --repository=hostdir/binpkgs xtools
+
+./xbps-src pkg intel-ucode
+sudo xbps-install -y --repository=hostdir/binpkgs/nonfree intel-ucode
+sudo xbps-reconfigure -f linux
+
+cd ~/
+
 # audio
 sudo xbps-install -Sy alsa-utils
 sudo touch /etc/asound.conf
 sudo echo "defaults.pcm.card 1" >> /etc/asound.conf
 sudo echo "defaults.pcm.device 0" >> /etc/asound.conf
 sudo echo "defaults.ctl.card 1" >> /etc/asound.conf
+sudo ln -s /etc/sv/alsa /var/service
 
 # track pad
+sudo touch /etc/dracut.conf.d/10-touchpad.conf
 sudo echo "add_drivers+=\"bcm5974\"" > /etc/dracut.conf.d/10-touchpad.conf
 sudo dracut --force
 
@@ -21,18 +51,13 @@ sudo ln -s /etc/sv/acpid /var/service/
 
 # powersave
 sudo xbps-install -Sy thermald powertop
-sudo ln -s /etcsv/thermald /var/service
+sudo ln -s /etc/sv/thermald /var/service/
 sudo powertop -auto-tune
-
-# microcode updates
-cd ~/void-packages
-./void-packages pkg intel-ucode
-sudo xbps-install -y --repository=hostdir/binpkgs/nonfree intel-ucode
-sudo xbps-reconfigure -f linux
-cd ~/
 
 # fun stuff
 sudo xbps-install -Sy \
+    i3-gaps \
+    zsh \
     base-devel \
     xorg-server \
     xorg-minimal \
@@ -40,7 +65,6 @@ sudo xbps-install -Sy \
     xf86-video-intel \
     xf86-input-libinput \
     picom \
-    dejavu-font-ttf \
     noto-fonts-ttf \
     nerd-fonts-ttf \
     font-awesome \
@@ -48,17 +72,37 @@ sudo xbps-install -Sy \
     qutebrowser \
     kitty \
     feh \
+    dbus \
+    iwd \
     dmenu \
     exa
+
+touch ~/.zshrc
+echo "HISTFILE=~/.histfile" >> ~/.zshrc
+echo "HISTSIZE=1000" >> ~/.zshrc
+echo "SAVEHIST=1000" >> ~/.zshrc
+echo "zstyle :compinstall filename '/home/waffles/.zshrc'"
+echo "autoload -Uz compinit promptinit" >> ~/.zshrc
+echo "compinit" >> ~/.zshrc
+chsh -s /bin/zsh
 
 sudo xbps-install -Sy nodejs-lts
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
-# Add to .bash_profile
 export PATH=~/.npm-global/bin:$PATH
 source ~/.bash_profile
+echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.zshrc
 
-sudo ln -sf /etc/sv/alsa /var/service
-sudo ln -sf /etc/sv/dbus /var/service
+mkdir -p ~/.config/kitty
+touch ~/.config/kitty/kitty.conf
+echo "font_family Fira Code Regular Nerd Font Complete" >> ~/.config/kitty/kitty.conf
+echo "font_size 10" >> ~/.config/kitty/kitty.conf
 
+mkdir -p ~/.config/nvim
 
+mkdir -p ~/.config/picom
+sudo cp -rf ~/usr/share/examples/picom/picom.sample.conf ~/.config/picom/picom.conf
+
+mkdir ~/.xinitrc
+echo "picom -b" >> ~/.xinitrc
+echo "exec i3" >> ~/.xinitrc
